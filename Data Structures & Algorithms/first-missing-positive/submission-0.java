9874b81903f186fdc22622e15a17c86fec07ed2f
Karpagavallie <karpagammarappan@gmail.com>
2026-08-27 16:10:30 +0530

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int ind=1;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]<ind)
+            {
+                continue;
+            }
+            if(nums[i]==ind)
+            {
+                ind++;
+            }
+
+        }
+        return ind;
+        
+    }
+}
