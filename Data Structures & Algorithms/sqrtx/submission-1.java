262869a66d660588136a3f06ba00199b60911d40
Karpagavallie <karpagammarappan@gmail.com>
2026-09-05 19:25:02 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int mySqrt(int x) {
+      int left=0;
+      int right=x/2;
+      if(x==1)
+      {
+        return 1;
+      }
+      while(left<=right)
+      {
+        int mid=left+(right-left)/2;
+        long square=(long)mid*mid;
+        if(square==x)
+        {
+            return mid;
+        }
+        else if(square<x)
+        {
+            left=mid+1;
+        }
+        else
+        {
+            right=mid-1;
+        }
+      }
+      return right;
+        
+    }
+}
